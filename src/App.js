c75c704232c17b9078677d3354255d8b55be7621
Olivia Robinson <thelibster123@gmail.com>
2020-07-30 16:32:35 +1200

    checklist 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.tasks = [
+      {
+        id: 1,
+        content: 'Clean room'
+      },
+      {
+        id: 2,
+        content: 'Do washing'
+      },
+      {
+        id: 3,
+        content: 'Make cake'
+      },
+    ]
+  }
+
+  render(){
+    return (
+      <div className="wrap">
+        <header>
+            My to do list
+        </header>
+        <main>
+            <div className="main-container">
+
+                {
+                  this.tasks.map((task)=>{
+
+                    return (
+                      <div className="task-group">
+                          <input type="checkbox" id="task" name="task"/>
+                          <label for="task">{task.content}</label>
+                          <button><i className="fas fa-times"></i></button>
+                      </div>
+                    )
+                  })
+                }
+       
+                <div className="new-task-group">
+                    <form>
+                        <button type="submit"><i className="fas fa-plus"></i></button>
+                        <div className="form-group">
+                            <input type="text" placeholder="Add new task"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
